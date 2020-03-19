99e755c6d0f3c6e2e60a247dbb88dd6cd36575e7
apurvtri123 <apurvtri123@gmail.com>
2020-03-19 20:39:34 +0100

    reverting back test commit

--- 11012505_Apurv_Trivedi.py
+++ 11012505_Apurv_Trivedi.py
@@ -28,7 +28,7 @@ print(result)
 #5
 
 a5 = np.array([1,2,3,2,3,4,3,4,5,6])
-b5 = np.array([7,2,10,2,7,4,9,4,9,10])
+b5 = np.array([7,2,10,2,7,4,9,4,9,8])
 result = np.where(a5 == b5)
 print(result)
 
